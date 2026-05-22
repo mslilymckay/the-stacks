660821b06de973bc181daadd56e7e14fd77cccca
mslilymckay <lily.morin423@gmail.com>
2026-05-22 14:03:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
+const supabaseUrl = 'https://jvsjzlvabtffhsnvmcto.supabase.co';
+const supabaseKey = 'sb_publishable_H2EPwvAaziQVz8T4yExdEw_bQrB5f3V';
+const supabase = createClient(supabaseUrl, supabaseKey);
